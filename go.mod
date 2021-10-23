5de2d82cfc3dca02401e4001a5ed7aa8cffb9553
Sanzhar <190103205@stu.sdu.edu.kz>
2021-10-23 18:04:48 +0600

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/h-u-m-a-n/main_test
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/h-u-m-a-n/lib_test"
+)
+
+func main() {
+	a, b := 1, 4
+	c := sum.Sum(a,b)
+	fmt.Println(c)
+}
+
